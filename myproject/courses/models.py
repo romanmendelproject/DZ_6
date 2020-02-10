adb960b7a3928b56a8fde8a1df0545b867f8c455
romanmendelproject <33754158+romanmendelproject@users.noreply.github.com>
2020-02-11 01:26:10 +0300

    correction of comments

--- myproject/courses/models.py
+++ myproject/courses/models.py
@@ -1,13 +1,23 @@
 from django.contrib.auth.models import User
 from django.db import models
-
-
-class Teacher(models.Model):
-    SEX_CHOICES = (('m', 'Men'),('f', 'Women'))
+from datetime import date
+class PersonMixin(models.Model):
+    SEX_MAN = 0
+    SEX_WOMAN = 1
+    SEX_CHOICES = (
+    (SEX_MAN, 'Man'),
+    (SEX_WOMAN, 'Woman')
+    )
+    sex = models.PositiveSmallIntegerField(choices=SEX_CHOICES, default=SEX_MAN)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    sex = models.CharField(max_length=1, choices=SEX_CHOICES)
-    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, default=date.today)
     city = models.CharField(max_length=50)
+
+    class Meta:
+        abstract = True
+
+
+class Teacher(PersonMixin):
     bio = models.CharField(max_length=1000)
 
     @property
@@ -18,12 +28,7 @@ class Teacher(models.Model):
         return self.full_name
 
 
-class Student(models.Model):
-    SEX_CHOICES = (('m', 'Men'), ('f', 'Women'))
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    sex = models.CharField(max_length=1, choices=SEX_CHOICES)
-    date_of_birth = models.DateField(blank=True)
-    city = models.CharField(max_length=50)
+class Student(PersonMixin):
 
     @property
     def full_name(self):
@@ -35,8 +40,8 @@ class Student(models.Model):
 
 class Course(models.Model):
     title = models.CharField(max_length=50)
-    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
-    students = models.ManyToManyField(Student)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, related_name="course")
+    students = models.ManyToManyField(Student, related_name="course")
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
@@ -46,8 +51,8 @@ class Course(models.Model):
 class Lesson(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=50, blank=True)
-    date = models.DateField(blank=True, null=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date = models.DateField(blank=True, default=date.today)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lesson")
 
     def __str__(self):
         return self.title
